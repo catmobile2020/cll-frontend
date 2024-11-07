7b14dc11de848b0d0c560fae7560f3ee71db8f32
Marwa Mahmoud <marwa@cyberx.world>
2024-11-07 14:58:18 +0200

    centeralize logo and remove janssen logo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
   <div>
     <div class="container wrapper">
-      <div class="w-full px-4 flex justify-between items-center mb-6">
-        <!-- Left Logo -->
-        <div class="logos-dimintions">
-          <img
-            src="../public/conf-logo.png"
-            alt="Left Logo"
-            class="h-15 w-[250px]"
-            
-          />
-        </div>
-
-        <!-- Right Logo -->
-        <div class="logos-dimintions">
-          <img
-            src="../public/logo1.png"
-            alt="Right Logo"
-            class="h-15  sm:w-[150px] w-[100px]"
-          />
-        </div>
+      <div class="flex justify-center items-center mb-6 mt-6 px-4">
+        <img
+          src="../public/conf-logo.png"
+          alt="Centered Logo"
+          class="h-15 w-[250px]"
+        />
       </div>
+      
       <router-view></router-view>
     </div>
   </div>
@@ -39,7 +27,6 @@ export default {
   }
 }
 #app {
- 
   font-family: "Verdana", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
